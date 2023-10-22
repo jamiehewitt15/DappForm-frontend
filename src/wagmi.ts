5dee8f07de3a6f594eb73c61d928b84a3a022830
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-10-22 12:12:11 +0300

    Updating chains

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,12 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createConfig } from 'wagmi'
-import { polygonMumbai, mainnet } from 'wagmi/chains'
+import { polygonMumbai, polygon } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 require('dotenv').config()
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [polygonMumbai] : [])],
+  [polygon, ...(process.env.NODE_ENV === 'development' ? [polygonMumbai] : [])],
   [
     alchemyProvider({
       apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
