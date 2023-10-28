4cb3bf2ed1b2b6368943e1a953cd8033a2f08f0e
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-10-28 17:00:03 +0300

    Removing console logs

--- src/components/shared/Permission.tsx
+++ src/components/shared/Permission.tsx
@@ -27,27 +27,22 @@ export default function Permission({
   const [orgId, setOrgId] = useState<string>('0')
   const [collectionId, setCollectionId] = useState<string>('0')
   const [documentId, setDocumentId] = useState<string>('0')
-  console.log('orgId', orgId)
-  console.log('collectionId', collectionId)
-  console.log('documentId', documentId)
 
   const router = useRouter()
   const path = router.asPath
-  console.log('path', path)
-  console.log('scope', scope)
 
   const pathArray = path
     .split('/')
     .filter((item) => item !== '' && !isNaN(Number(item)))
-  console.log('pathArray', pathArray)
 
   const adminPermission = orgAdmin({
     args: [BigInt(orgId), address]
   }).data
+
   const collectionPermission = collectionPublisher({
     args: [BigInt(orgId), BigInt(collectionId), address]
   }).data
-  console.log('collectionPermission', collectionPermission)
+
   const documentPermission = documentUpdator({
     args: [BigInt(orgId), BigInt(collectionId), BigInt(documentId), address]
   }).data
@@ -57,25 +52,15 @@ export default function Permission({
       let newPermission = false
       switch (scope) {
         case 'admin':
-          console.log('checking if user is org admin')
           setOrgId(paramOrgId ? paramOrgId : pathArray[0])
           newPermission = adminPermission ? adminPermission : false
           break
         case 'publisher':
-          console.log(
-            'checking if user is collection publisher',
-            collectionPermission
-          )
           setOrgId(paramOrgId ? paramOrgId : pathArray[0])
           setCollectionId(paramCollectionId ? paramCollectionId : pathArray[1])
           newPermission = collectionPermission ? collectionPermission : false
-          console.log('collection permission', permission)
           break
         case 'updator':
-          console.log(
-            'checking if user is document updator',
-            documentPermission
-          )
           setOrgId(paramOrgId ? paramOrgId : pathArray[0])
           setCollectionId(paramCollectionId ? paramCollectionId : pathArray[1])
           setDocumentId(paramDocumentId ? paramDocumentId : pathArray[2])
@@ -85,10 +70,8 @@ export default function Permission({
           setPermission(false)
       }
       setPermission(newPermission ? newPermission : false)
-      console.log('permission', permission)
     }
   }, [pathArray, adminPermission, collectionPermission, documentPermission])
 
-  console.log('END permission', permission)
   return <>{permission ? children : null}</>
 }
