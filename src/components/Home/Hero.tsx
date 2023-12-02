7b373d3eab6387b3f2af92d71d6d33476a5a5519
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-12-02 19:47:00 +0300

    Adding logos onto the hero section

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Box, Typography, Button } from '@mui/material'
+import { Box, Typography, Button, Link } from '@mui/material'
 import styles from './heroStyles.module.css'
 import Calendly from '@components/shared/CalendlyWithArrow'
+import Image from 'next/image'
 
 export default function Hero() {
   return (
@@ -20,7 +21,37 @@ export default function Hero() {
         </Typography>
         <Calendly />
       </Box>
+      <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
+        <Typography variant="body1" component="h2">
+          Powered by:
+        </Typography>
+        <Box display="flex" justifyContent="center" mt={-2}>
+          <Link
+            href="https://polygon.technology/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/Polygon.svg"
+              alt="Polygon Logo"
+              width={150}
+              height={100} // Adjust the height as needed to maintain the aspect ratio
+            />
+          </Link>
+          <Link
+            href="https://thegraph.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/TheGraph.svg"
+              alt="The Graph Logo"
+              width={100}
+              height={100} // Adjust the height as needed to maintain the aspect ratio
+            />
+          </Link>
+        </Box>
+      </Box>
     </Box>
   )
 }
-// Let's increase transparency and earn trust.
