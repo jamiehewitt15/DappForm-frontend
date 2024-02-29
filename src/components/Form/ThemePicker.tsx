1e25b71fd61b10cbf912bf091c0316e36b68204b
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-02-29 13:30:58 +0300

    Updating theme selector draw styling + adding close button

--- src/components/Form/ThemePicker.tsx
+++ src/components/Form/ThemePicker.tsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import CheckIcon from '@mui/icons-material/Check'
+import CloseIcon from '@mui/icons-material/Close'
 import { HexColorPicker, HexColorInput } from 'react-colorful'
 import PaletteIcon from '@mui/icons-material/Palette'
-import { Box, IconButton, SvgIcon, Typography, Drawer } from '@mui/material'
+import {
+  Box,
+  IconButton,
+  SvgIcon,
+  Typography,
+  Drawer,
+  Divider,
+  Stack
+} from '@mui/material'
 import { lightenColor } from '@utils/backgroundColor'
 import FontSelector from './FontSelector'
 
@@ -58,66 +67,82 @@ export default function ThemePicker({
         <PaletteIcon sx={{ width: 30, height: 30, marginRight: '10px' }} />
       </IconButton>
       <Drawer anchor="right" open={isDrawerOpen} onClose={handleDrawerClose}>
-        <div style={{ width: 'auto', padding: '20px' }}>
-          <FontSelector />
-          <Typography variant="h6">Choose Color:</Typography>
-          <HexColorPicker color={color} onChange={changeColor} />
-          #<HexColorInput color={color} onChange={changeColor} />
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '10px',
-              marginTop: '10px'
+        <Box sx={{ p: 3 }}>
+          <IconButton
+            onClick={handleDrawerClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8
             }}
           >
-            <p style={{ margin: 0 }}>Selected Color:</p>
-            <div
-              style={{
-                width: '24px',
-                height: '24px',
-                borderRadius: '50%',
-                background: color,
-                border: '1px solid #5A5A5A' // Adds a border to make the circle more visible
-              }}
-            ></div>
-          </div>
-          <h4>Choose Background:</h4>
-          <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
-            {colorOptions.map((option) => (
-              <IconButton
-                key={option.label}
-                onClick={() => changeBackgroundColor(option.hex)}
-                sx={{
-                  position: 'relative',
-                  width: 24,
-                  height: 24,
-                  borderRadius: '50%',
-                  backgroundColor: option.hex,
-                  '&:hover': {
-                    backgroundColor: option.hex
-                  },
-                  border: '1px solid #5A5A5A' // Adds a border to make the circle more visible
+            <CloseIcon />
+          </IconButton>
+          <Stack spacing={2}>
+            <div style={{ width: 'auto', padding: '20px' }}>
+              <FontSelector />
+              <Divider style={{ marginTop: 20, marginBottom: 20 }} />
+              <Typography variant="h6">Choose Color:</Typography>
+              <HexColorPicker color={color} onChange={changeColor} />
+              #<HexColorInput color={color} onChange={changeColor} />
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '10px',
+                  marginTop: '10px'
                 }}
               >
-                {backgroundColor === option.hex && (
-                  <SvgIcon
+                <p style={{ margin: 0 }}>Selected Color:</p>
+                <div
+                  style={{
+                    width: '24px',
+                    height: '24px',
+                    borderRadius: '50%',
+                    background: color,
+                    border: '1px solid #5A5A5A' // Adds a border to make the circle more visible
+                  }}
+                ></div>
+              </div>
+              <Divider style={{ marginTop: 20, marginBottom: 20 }} />
+              <Typography variant="h6">Choose Background:</Typography>
+              <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+                {colorOptions.map((option) => (
+                  <IconButton
+                    key={option.label}
+                    onClick={() => changeBackgroundColor(option.hex)}
                     sx={{
-                      color: 'black', // Adjust color based on what looks good with your color choices
-                      position: 'absolute',
-                      top: 0,
-                      left: 0,
-                      width: '100%',
-                      height: '100%'
+                      position: 'relative',
+                      width: 24,
+                      height: 24,
+                      borderRadius: '50%',
+                      backgroundColor: option.hex,
+                      '&:hover': {
+                        backgroundColor: option.hex
+                      },
+                      border: '1px solid #5A5A5A' // Adds a border to make the circle more visible
                     }}
                   >
-                    <CheckIcon />
-                  </SvgIcon>
-                )}
-              </IconButton>
-            ))}
-          </Box>
-        </div>
+                    {backgroundColor === option.hex && (
+                      <SvgIcon
+                        sx={{
+                          color: 'black', // Adjust color based on what looks good with your color choices
+                          position: 'absolute',
+                          top: 0,
+                          left: 0,
+                          width: '100%',
+                          height: '100%'
+                        }}
+                      >
+                        <CheckIcon />
+                      </SvgIcon>
+                    )}
+                  </IconButton>
+                ))}
+              </Box>
+            </div>
+          </Stack>
+        </Box>
       </Drawer>
     </div>
   )
