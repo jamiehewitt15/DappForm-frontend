93f07a72b0c79dc5d1f3cb514565ab92ad6344b7
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-05-07 15:58:31 +0300

    Creating component for preparing to publish document

---
+++ src/components/Publish/submitForm.tsx
@@ -0,0 +1,45 @@
+import { ReactElement, ReactNode } from 'react'
+import Form from '@components/Form'
+import {
+  useAltBaseGetFees as getFees,
+  usePrepareAltBasePublishOrUpdateDocument as prepareDocument
+} from '@hooks/generated'
+import { useFormContext } from '@context/FormContext'
+
+export default function CreateOrEditForm({
+  children
+}: {
+  children: ReactNode
+}): ReactElement {
+  const { fieldNames, fieldDataTypes, orgId, collectionId } = useFormContext()
+
+  // TODO: at some point we will want to allow users to update their form responses
+  const documentStatus = {
+    update: false,
+    retired: false
+  }
+
+  const allFees = getFees().data
+  const fee = allFees ? allFees[2] : undefined
+
+  const { config } = prepareDocument({
+    args: [
+      {
+        documentId: 0,
+        organisationId: orgId,
+        collectionId: collectionId,
+        fieldNames: fieldNames,
+        fieldDataTypes: fieldDataTypes,
+        fieldValues: [],
+        status: documentStatus
+      }
+    ],
+    value: fee
+  })
+
+  return (
+    <Form successPath={'/organisation/' + orgId} config={config}>
+      {children}
+    </Form>
+  )
+}
