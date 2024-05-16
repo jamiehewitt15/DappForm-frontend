afc5843a87aa817bb8ad8fcd1766d31236e1bbbb
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-05-17 00:49:51 +0300

    Fix: mobile optimisation

--- src/pages/form/[id].tsx
+++ src/pages/form/[id].tsx
@@ -6,7 +6,7 @@ import { Box } from '@mui/material'
 import UserThemeProvider from '@context/UserThemeProvider'
 import { useFormContext } from '@context/FormContext'
 
-export default function ditForm() {
+export default function DitForm() {
   const { setCollectionId } = useFormContext()
   const router = useRouter()
 
@@ -26,7 +26,18 @@ export default function ditForm() {
   }, [router.query.id])
 
   return (
-    <Box sx={{ maxWidth: '1280px', margin: 'auto', padding: '3rem' }}>
+    <Box
+      sx={{
+        maxWidth: '1280px',
+        margin: 'auto',
+        padding: {
+          xs: '1rem', // Padding for mobile devices
+          sm: '2rem', // Padding for tablets
+          md: '3rem' // Padding for larger screens
+        },
+        width: '100%' // Ensures the Box takes the full width on mobile devices
+      }}
+    >
       <UserThemeProvider>
         <SubmitForm>
           <Form />
