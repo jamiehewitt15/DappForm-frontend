99df71db1db50f5d520f58c5368f165e44830755
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-05-17 01:07:12 +0300

    Fix: mobile optimisation

--- src/components/Home/Features.tsx
+++ src/components/Home/Features.tsx
@@ -11,42 +11,46 @@ export default function CallToAction() {
   ]
 
   return (
-    <Card sx={{ m: 6, p: 5 }}>
-      <Typography variant="h1" align="center" sx={{ m: 4, mb: 8 }}>
+    <Card
+      sx={{
+        m: {
+          xs: 2, // Margin for mobile devices
+          sm: 4, // Margin for tablets
+          md: 6 // Margin for larger screens
+        },
+        p: {
+          xs: 2, // Padding for mobile devices
+          sm: 4, // Padding for tablets
+          md: 5 // Padding for larger screens
+        }
+      }}
+    >
+      <Typography
+        variant="h4" // Smaller variant for mobile devices
+        align="center"
+        sx={{
+          m: {
+            xs: 2, // Margin for mobile devices
+            sm: 3, // Margin for tablets
+            md: 4 // Margin for larger screens
+          },
+          mb: {
+            xs: 4, // Margin bottom for mobile devices
+            sm: 6, // Margin bottom for tablets
+            md: 8 // Margin bottom for larger screens
+          },
+          fontSize: {
+            xs: '1.5rem', // Font size for mobile devices
+            sm: '2rem', // Font size for tablets
+            md: '2.5rem' // Font size for larger screens
+          }
+        }}
+      >
         Features
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Box display="flex" justifyContent="center">
-            <StarList items={items} />
-          </Box>
-        </Grid>
-        <Grid
-          item
-          xs={6}
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          <Box
-            style={{
-              border: '1px solid black',
-              padding: '8px',
-              borderRadius: '8px'
-            }}
-          >
-            <Image
-              src="/altbase-screenshot.png"
-              alt="Screenshot"
-              width={500} // Adjust the width as needed
-              height={300} // Adjust the height as needed
-              layout="intrinsic"
-            />
-          </Box>
-        </Grid>
-      </Grid>
+      <Box display="flex" justifyContent="center">
+        <StarList items={items} />
+      </Box>
     </Card>
   )
 }
