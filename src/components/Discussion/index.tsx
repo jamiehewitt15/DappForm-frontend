a12c057e585f3b853f803b6a23d01520c3b6d96e
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-05-25 22:14:04 +0300

    Adding text field for responses

--- src/components/Discussion/index.tsx
+++ src/components/Discussion/index.tsx
@@ -1,10 +1,34 @@
 import { ReactElement } from 'react'
 import ResponseFormHeading from '../ResponseForm/ResponseFormHeading'
+import { Box, Card, CardContent, Typography, TextField } from '@mui/material'
 
 export default function Form(): ReactElement {
   return (
     <>
-      <ResponseFormHeading />
+      <ResponseFormHeading showRequiredMessage={false} />
+      <Card
+        key="respnse"
+        sx={{
+          mt: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          boxShadow: 1,
+          borderRadius: '8px'
+        }}
+      >
+        <CardContent sx={{ pb: 0, pt: 1 }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              mb: 2
+            }}
+          ></Box>
+          <TextField label="Response" fullWidth multiline rows={4} />
+        </CardContent>
+      </Card>
     </>
   )
 }
