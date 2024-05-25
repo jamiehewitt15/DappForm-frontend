6e272c76e1b4723a08d465cfb03ff09375c8c492
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-05-25 22:00:35 +0300

    Creating discussion page

---
+++ src/components/Discussion/index.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from 'react'
+import ResponseFormHeading from '../ResponseForm/ResponseFormHeading'
+
+export default function Form(): ReactElement {
+  return (
+    <>
+      <ResponseFormHeading />
+    </>
+  )
+}
---
+++ src/pages/ResponseLayout.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import SubmitForm from '@components/Publish/SubmitForm'
+import { Box } from '@mui/material'
+import UserThemeProvider from '@context/UserThemeProvider'
+import { useFormContext } from '@context/FormContext'
+
+export default function ResponseLayout({ children }) {
+  const { setCollectionId } = useFormContext()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (
+      router.isReady &&
+      router.query.id &&
+      typeof router.query.id === 'string'
+    ) {
+      if (router.query.id.startsWith('0x')) {
+        setCollectionId(router.query.id)
+      } else {
+        const id = parseInt(router.query.id, 10)
+        !isNaN(id) && setCollectionId(id)
+      }
+    }
+  }, [router.query.id])
+
+  return (
+    <Box
+      sx={{
+        maxWidth: '1280px',
+        margin: 'auto',
+        padding: {
+          xs: '1rem', // Padding for mobile devices
+          sm: '2rem', // Padding for tablets
+          md: '3rem' // Padding for larger screens
+        },
+        width: '100%' // Ensures the Box takes the full width on mobile devices
+      }}
+    >
+      <UserThemeProvider>
+        <SubmitForm>{children}</SubmitForm>
+      </UserThemeProvider>
+    </Box>
+  )
+}
