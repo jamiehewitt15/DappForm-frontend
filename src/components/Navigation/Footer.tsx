9ac1208d981d33ffd95f04dff6c2796d14a19186
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-05-28 17:22:01 +0300

    Updating contract email address

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -72,7 +72,7 @@ export default function Footer() {
               <Typography variant="h6">Contact</Typography>
               <IconButton
                 color="inherit"
-                href="mailto:support@altbase.io"
+                href="mailto:support@dappform.io"
                 target="_blank"
                 rel="noopener"
               >
