ace0c19d1ec9facafb11663c0c9dbd5db304ecba
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-05-28 11:25:09 +0300

    Updating logo styling

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -85,7 +85,7 @@ export default function NavBar() {
                       <Image
                         src="/logo-transparent-svg.svg"
                         alt="AltBase Logo"
-                        width={350}
+                        width={200}
                         height={60}
                       />
                     </div>
