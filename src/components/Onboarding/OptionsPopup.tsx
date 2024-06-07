64a75fa6ce16d9cbe5e9ac2e08cbbdc86d6d0cd6
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-06-07 14:27:53 +0300

    Fixing backdrop to ensure it closes the dialoge

--- src/components/Onboarding/OptionsPopup.tsx
+++ src/components/Onboarding/OptionsPopup.tsx
@@ -39,68 +39,66 @@ export default function OptionsPopup({ children }: OptionsPopupProps) {
     <div onClick={handleClickOpen}>
       {children}
       <Backdrop open={open} style={{ zIndex: 1300 }} onClick={handleClose}>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          onClick={(e) => e.stopPropagation()}
-        >
-          <Box padding={2} position="relative">
-            <IconButton
-              aria-label="close"
-              onClick={handleClose}
-              style={{ position: 'absolute', right: 8, top: 8 }}
-            >
-              <CloseIcon />
-            </IconButton>
-            <DialogTitle>Create a:</DialogTitle>
-            <DialogContent>
-              <Grid
-                container
-                spacing={2}
-                justifyContent="center"
-                alignItems="center"
+        <div onClick={(e) => e.stopPropagation()}>
+          <Dialog open={open} onClose={handleClose}>
+            <Box padding={2} position="relative">
+              <IconButton
+                aria-label="close"
+                onClick={handleClose}
+                style={{ position: 'absolute', right: 8, top: 8 }}
               >
-                <Grid item xs={6}>
-                  <Box
-                    display="flex"
-                    flexDirection="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    border={1}
-                    borderRadius={4}
-                    padding={4}
-                    style={{ cursor: 'pointer' }}
-                    onClick={() => handleRedirect('/start/form/0')}
-                  >
-                    <FormIcon style={{ fontSize: 50 }} />
-                    <Typography
-                      variant="h6"
-                      sx={{ textDecoration: 'none', color: 'inherit' }}
+                <CloseIcon />
+              </IconButton>
+              <DialogTitle>Create a:</DialogTitle>
+              <DialogContent>
+                <Grid
+                  container
+                  spacing={2}
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Grid item xs={6}>
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                      justifyContent="center"
+                      border={1}
+                      borderRadius={4}
+                      padding={4}
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleRedirect('/start/form/0')}
                     >
-                      Form
-                    </Typography>
-                  </Box>
+                      <FormIcon style={{ fontSize: 50 }} />
+                      <Typography
+                        variant="h6"
+                        sx={{ textDecoration: 'none', color: 'inherit' }}
+                      >
+                        Form
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                      justifyContent="center"
+                      border={1}
+                      borderRadius={4}
+                      padding={4}
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleRedirect('/start/discussion/0')}
+                    >
+                      <DiscussionIcon style={{ fontSize: 50 }} />
+                      <Typography variant="h6">Discussion</Typography>
+                    </Box>
+                  </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                  <Box
-                    display="flex"
-                    flexDirection="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    border={1}
-                    borderRadius={4}
-                    padding={4}
-                    style={{ cursor: 'pointer' }}
-                    onClick={() => handleRedirect('/start/discussion/0')}
-                  >
-                    <DiscussionIcon style={{ fontSize: 50 }} />
-                    <Typography variant="h6">Discussion</Typography>
-                  </Box>
-                </Grid>
-              </Grid>
-            </DialogContent>
-          </Box>
-        </Dialog>
+              </DialogContent>
+            </Box>
+          </Dialog>
+        </div>
       </Backdrop>
     </div>
   )
